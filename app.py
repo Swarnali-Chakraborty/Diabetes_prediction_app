ec7f673148c00d30a9e3d2569b0271037015c9b4
Swarnali-Chakraborty <cswarnali3@gmail.com>
2025-07-01 12:07:34 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,10 +9,21 @@ import seaborn as sns
 #model = pickle.load(open('svm_model.sav', 'rb'))  # main prediction model (SVM)
 
 # Model selector
+#model_choice = st.selectbox(
+    #"🧠 Choose a Model for Prediction",
+  #["🧩 SVM", "🌲 Random Forest", "📊 Logistic Regression", "👥 KNN"]
+#)
 model_choice = st.selectbox(
-    "🧠 Choose a Model for Prediction",
-  ["🧩 SVM", "🌲 Random Forest", "📊 Logistic Regression", "👥 KNN"]
+    "🧠 Choose Prediction Method (Model)",
+    [
+        "💻 Basic (Support Vector Machine - SVM)",
+        "🌲 Advanced (Random Forest)",
+        "📊 Classic (Logistic Regression)",
+        "👥 Nearest Match (K-Nearest Neighbors - KNN)"
+    ],
+    help="Different methods used by the app to predict diabetes. You can try each one!"
 )
+st.markdown("### 👤 Personal & Health Details (Please fill accurately)")
 
 # Load the selected model
 if "SVM" in model_choice:
@@ -38,17 +49,18 @@ st.subheader("📥 Enter Patient Data")
 # Input fields
 col1, col2 = st.columns(2)
 with col1:
-  Pregnancies = st.number_input('Pregnancies', min_value=0, step=1)
-  Glucose = st.number_input('Glucose Level', min_value=0)
-  BloodPressure = st.number_input('Blood Pressure', min_value=0)
-  SkinThickness = st.number_input('Skin Thickness', min_value=0)
+  Pregnancies = st.number_input('Pregnancies (count)', min_value=0,
+  step=1,help="Enter how many times the person has been pregnant (0 if never)")
+  Glucose = st.number_input('Glucose Level (mg/dL)', min_value=0,help="Enter fasting blood sugar level. Normal is around 70–100 mg/dL")
+  BloodPressure = st.number_input('Blood Pressure (mm Hg)', min_value=0,help="Diastolic blood pressure (bottom number). Normal is around 80 mm Hg")
+  SkinThickness = st.number_input('Skin Thickness (mm)', min_value=0,help="Skin thickness near tricep muscle. Measured using calipers")
 
 
 with col2:
-  Insulin = st.number_input('Insulin', min_value=0)
-  BMI = st.number_input('BMI', min_value=0.0)
-  DPF = st.number_input('Diabetes Pedigree Function', min_value=0.0)
-  Age = st.number_input('Age', min_value=1)
+  Insulin = st.number_input('Insulin (mu U/mL)', min_value=0,help="2-hour serum insulin level. Normal range is around 16–166 mu U/mL")
+  BMI = st.number_input('BMI (kg/m²)', min_value=0.0, help="BMI = weight/height². Normal: 18.5 to 24.9")
+  DPF = st.number_input('Diabetes Pedigree Function (ratio)', min_value=0.0,help="Likelihood of diabetes based on family history (0 to 2+)")
+  Age = st.number_input('Age(years)', min_value=1, help="Enter the person’s age in years")
 
 # Prediction logic
 if st.button('Predict Diabetes'):
